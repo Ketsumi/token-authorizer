42873cb40a11adf13e44b1af2097c77948648039
ketsumi <mewtropolis.ketsumi@gmail.com>
2019-04-30 17:46:15 -0400

    generateToken and decodeToken methods created

---
+++ authorizer.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'SuperSecretKey'
+const authorizer = {};
+
+authorizer.generateToken = payload => {
+	const token = jwt.sign(payload, SECRET_KEY);
+
+	return token;
+};
+
+authorizer.decodeToken = token => {
+	try {
+		const decoded = jwt.verify(token, SECRET_KEY);
+
+		return decoded;
+	} catch(err) {
+		console.log(err);
+
+		return null;
+	}
+};
+
+module.exports = authorizer;
---
+++ handler.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.authorizer = async (ev, ct, cb) => {
+	// stuff
+}
