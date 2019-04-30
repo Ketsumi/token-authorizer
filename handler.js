6431d83710f3bf7f250813575a9f061da9ae5cbd
ketsumi <mewtropolis.ketsumi@gmail.com>
2019-04-30 18:51:38 -0400

    handler completed

--- handler.js
+++ handler.js
@@ -1,5 +1,23 @@
 'use strict';
 
-exports.authorizer = async (ev, ct, cb) => {
-	// stuff
+const authorizer = require('./authorizer');
+const policy = require('./policy');
+
+exports.handler = async (ev, ct, cb) => {
+	const payload = ev.body;
+	const token = ev.authorizationToken;
+	
+	if (!token) {
+		return generateToken(ev.body);
+	} else {
+		return policy.generatePolicy(token, ev.methodArn);
+	}
+}
+
+function generateToken(payload) {
+	return authorizer.generateToken(payload);
+}
+
+function generatePolicy(token, methodArn) {
+	return policy.generatePolicy(token, methodArn);
 }
